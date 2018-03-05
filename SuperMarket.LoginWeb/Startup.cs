8894a13a67359ba5f24a6acecdf8bb08cbe0b92a
oliverchina <lgzh306@126.com>
2018-03-05 22:00:32 +0800

    11

---
+++ SuperMarket.LoginWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SuperMarket.LoginWeb.Startup))]
+namespace SuperMarket.LoginWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ SuperMarket.SysWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuperMarket.SysWeb.MvcApplication" Language="C#" %>
